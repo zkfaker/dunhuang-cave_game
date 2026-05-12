bd77b2c9ff7fefab4b75300504ad3140a05db88c
zkfaker <1538830504@qq.com>
2026-05-12 15:15:53 +0800

    Set Gitee Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dunhuang-cave_game/",
+  base: "/dunhuang_257_cave_game/",
   plugins: [react()],
   server: {
     host: true,
