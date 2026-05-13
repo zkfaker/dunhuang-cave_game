aec9aa6c122b9744548f57dcef240c2c44cb2c01
zkfaker <1538830504@qq.com>
2026-05-13 21:33:55 +0800

    Localize loading title

--- src/components/LoadingOverlay.jsx
+++ src/components/LoadingOverlay.jsx
@@ -2,7 +2,7 @@ function LoadingOverlay({ progress = 0 }) {
   return (
     <div className="loading-overlay">
       <div className="loading-card">
-        <div className="loading-title">Loading cave scene</div>
+        <div className="loading-title">正在全力加载游戏画面</div>
         <div className="loading-progress">
           <span style={{ width: `${progress}%` }} />
         </div>
